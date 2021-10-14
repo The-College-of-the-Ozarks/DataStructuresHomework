d867f6cd995d628a96ea513d8b08af558bfc9c13
James Ferguson <77741224+242350@users.noreply.github.com>
2021-10-13 21:00:42 -0500

    Update test.py
    Added commands to import unittest and call the test (which doesn't exist yet)

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 #Have this program read an 'out.txt' file made by the student's python file
 #Use unit  testing to Assert that the string imported from the txt is equal to what we want (the values we want to match must be supplied at runtime)
+
+import unittest
+
+
+
+unittest.main()
