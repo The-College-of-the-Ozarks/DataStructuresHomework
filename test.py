203be67f68f96adbf837cf74832025fa626ba1fc
James Ferguson <77741224+242350@users.noreply.github.com>
2021-10-26 19:01:35 -0500

    Update test.py
    Changed .txt to .py, removed unneeded code, made notes about output type

--- test.py
+++ test.py
@@ -1,24 +1,20 @@
-#Have this program read an 'out.txt' file made by the student's python file
+#Have this program read an 'out.py' file made by the student's python file
 #Use unit  testing to Assert that the string imported from the txt is equal to what we want (the values we want to match must be supplied at runtime)
 
 import unittest
+import out
 
 class TestStudentSubmissions(unittest.TestCase):
   
   def __init__(self):
-    # I think I'll need the solution or ideal function in a file or something to import here, not sure though
-    self.control = open(answer_key.txt)
-  
+    # We will need to include a variable predicting the wanted output type within each assignment; for example: output_type = int
+    self.type = out.output_type
+    
   def setUp(self):
-    # Gonna need some info on where exactly this out.txt is coming from, I'm assuming I'm gonna need to access it through os
-    self.student = open(out.txt)
   
-  def test_student_files(self):
-    # So are we actually trying to run some functions inside of here and see if they come up with the same results? Or just if the files match?
-    self.assertEqual(self.student, self.control)
+  def test_integer(self):
     
   def tearDown(self):
-    self.fi.dispose()
     
 
 unittest.main()
