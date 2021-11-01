e7fb11784bd76f3bc74f8478c5d87e08913052ea
James Ferguson <77741224+242350@users.noreply.github.com>
2021-11-01 16:28:33 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 #Use unit  testing to Assert that the string imported from the txt is equal to what we want (the values we want to match must be supplied at runtime)
 
 import unittest
+import random
 import out
 
 class TestStudentSubmissions(unittest.TestCase):
@@ -10,9 +11,37 @@ class TestStudentSubmissions(unittest.TestCase):
   def test_multiply_two_integers(self):
     for i in range(20):
       with self.subTest(i=i):
-        a = random.randint(-10, 10)
+        a = random.randint(-10,10)
         b = random.randint(-10,10)
         self.assertEqual(out.func(a, b), a * b)
+        
+  def test_multiply_integers_with_args(self):
+    for i in range(5):
+      with self.subTest(i=i):
+        a = random.randint(-10,10)
+        self.assertEqual(out.func(a), a)
+    for j in range(5):
+      with self.subTest(j=j):
+        a = random.randint(-10,10)
+        b = random.randint(-10,10)
+        self.assertEqual(out.func(a, b), a * b)
+    for k in range(5):
+      with self.subTest(k=k):
+        a = random.randint(-10,10)
+        b = random.randint(-10,10)
+        c = random.randint(-10,10)
+        d = random.randint(-10,10)
+        e = random.randint(-10,10)
+        self.assertEqual(out.func(a, b, c, d, e), a * b * c * d * e)
+
+    def test_multiply_integers_with_args(self):
+      for i in range(10):
+        unsorted_list = []
+        n = random.randint(1, 30)
+        for j in range(n):
+          unsorted_list.append(random.randint(-50, 50))
+        self.assertEqual(out.func(unsorted_list), sorted(unsorted_list))
+     
     
 
 unittest.main()
